8353657a112870f02abe9c9ad17fb58a80349a8b
Koji NAKAMURA <kozy4324@gmail.com>
2013-07-22 17:00:59 +0900

    not to use Struc

--- spec/httpspec_simple/custom_matcher_spec.rb
+++ spec/httpspec_simple/custom_matcher_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe 'definition custom matchers' do
   it "should define the new matcher(not raise the error)" do
+    response = nil
+    server_start { response = request('/') }
     expect {
-      Struct.new("DummyRequest", :status).new("200").should be_http_ok
+      response.should be_http_ok
     }.not_to raise_error
   end
 end
